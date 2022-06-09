69d647bd8b793034d552e6df1a3f930474814698
Tiago Pinheiro <tiago.plemes@gmail.com>
2022-06-09 18:33:00 -0300

    Criacao telas de motorista e passageiro

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -1,11 +1,92 @@
 import React from "react";
 
-import { Container, Title } from '../../styles';
+import { Container, Title, SubTitle, Spacer, Map, Avatar, Input, Button, ButtonText, VerticalSeparator } from '../../styles';
+import { TouchableOpacity } from 'react-native';
 
 const Home = () => {
+
+    const tipo = 'P';
+    const status = 'I'; // S - SEM CORRIDA, I - INFORMAÇÕES, P - PESQUISA, C - CORRIDA
+
     return (
         <Container>
-            <Title>Home</Title>
+            <Map 
+                initialRegion={{
+                    latitude: -30.011364,
+                    longitude: -51.1637373,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            />
+            
+            <Container
+                position="absolute"
+                justify="space-between"
+                align="flex-start"
+                padding={20}
+                zIndex={999}
+                pointerEvents="box-none"
+                style={{height: '100%'}}
+            >
+                {/* PARTE SUPERIOR */}
+                <Container
+                    height={100}
+                    justify="flex-start"
+                    align="flex-start"
+                >
+                    <TouchableOpacity>
+                        <Avatar 
+                            source={{
+                                uri: 'https://www.jornalcontabil.com.br/wp-content/uploads/2022/03/motorista-de-caminhao-desistir-durante-a-pandemia-de-coronavirus_53419-9469.jpg'
+                            }}/>
+                    </TouchableOpacity>
+
+                </Container>
+                                
+                <Container elevation={50} height={150} color="light">
+
+                    {/* PASSAGEIRO SEM CORRIDA */}
+                    {tipo === "P" && status === "S" && 
+                        <Container justify="flex-start" padding={20} align="flex-start">
+                            <SubTitle>Olá, Luan Evangelista.</SubTitle>
+                            <Title>Para onde você quer ir?</Title>
+                            <Spacer />
+                            <Input placeholder="Procure um destino..."/>
+                        </Container>
+                    }
+
+                    {/* PASSAGEIRO INFORMAÇÕES DA CORRIDA */}
+                    {tipo === "P" && status === "I" && 
+                        <Container justify="flex-end" align="flex-start">
+                            <Container padding={20}>
+                                <SubTitle>DriverX Convencional</SubTitle>
+                                <Spacer />
+                                <Container row>
+                                    <Container>
+                                        <Title>R$ 13,90</Title>
+                                    </Container>
+                                    <VerticalSeparator />
+                                    <Container>
+                                        <Title>5 mins</Title>
+                                    </Container>
+                                </Container>
+                            </Container>
+                            <Button>
+                                <ButtonText>Chamar DriverX</ButtonText>
+                            </Button>
+                        </Container>
+                    }
+
+                    {/* MOTORISTA SEM CORRIDA */}
+                    {tipo === "M" && 
+                        <Container>
+                            <SubTitle>Olá, Juliana.</SubTitle>
+                            <Title>Nem uma corrida encontrada.</Title>
+                        </Container>
+                    }
+                </Container>
+
+            </Container>
         </Container>
     );
 }
--- app/src/styles/index.js
+++ app/src/styles/index.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components/native';
 import theme from './theme.json';
 
+import MapView from 'react-native-maps';
+
 export const Container = styled.View`
     flex: 1;
     background: ${(props) =>
@@ -78,4 +80,27 @@ export const AddressList = styled.FlatList`
 export const AddressItem = styled.TouchableOpacity`
     padding: 5px 0;
     align-items: flex-start;
+`;
+
+export const Map = styled(MapView)`
+    flex: 1;
+    width: 100%;
+    height: 100%;
+    opacity: ${(props) => (props.disabled ? 0.2 : 1)};
+`;
+
+export const Avatar = styled.Image.attrs({
+    elevation: 50,
+})`
+    width: ${(props) => (props.small ? '35px' : '50px')};
+    height: ${(props) => (props.small ? '35px' : '50px')};
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    background: ${theme.colors.muted};
+    border-radius: ${(props) => (props.small ? '35px' : '50px')};
+`; 
+
+export const VerticalSeparator = styled.View`
+    width: 1px;
+    height: 100%;
+    background-color: ${theme.colors.muted};
 `;
