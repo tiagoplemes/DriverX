4bba420578cbb8bcbf575065ca331c179a3f811a
Tiago Pinheiro <tiago.plemes@gmail.com>
2022-06-13 08:54:30 -0300

    Tentativa de Login com Facebook

--- app/index.js
+++ app/index.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 
-import Login from './src/pages/Login';
-import Type from './src/pages/Steps/type';
-import Car from './src/pages/Steps/car';
-import Payment from './src/pages/Steps/payment';
-import Ride from './src/pages/Ride';
-import Home from './src/pages/Home';
+import Routes from './src/routes';
+import { Provider } from 'react-redux';
+import store from './src/store';
 
-AppRegistry.registerComponent(appName, () => Home);
+const App = () => (
+    <Provider store={store}>
+        <Routes />
+    </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => App);
